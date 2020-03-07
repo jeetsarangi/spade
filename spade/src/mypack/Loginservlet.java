244cc2dde03360f0a48d00c5c9014302598c0bb9
Jeet <jeetsarangiflash@gmail.com>
2020-03-07 12:39:27 +0530

    Second commit after glitch updation in loginservlet get to post

--- spade/src/mypack/Loginservlet.java
+++ spade/src/mypack/Loginservlet.java
@@ -26,7 +26,7 @@ public class Loginservlet extends HttpServlet {
     	}
     }
 	
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		Connection conn=null;
 		PreparedStatement ps = null;
 		
